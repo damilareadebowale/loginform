65e5eb08c70c880d91509aa4e8dcdccd1b4a6e28
desmondadebowale <32304007+desmondadebowale@users.noreply.github.com>
2020-03-12 16:55:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function inputvalues() {
+    document.getElementById('display').innerHTML = document.getElementById("InputEmail").value;
+    document.getElementById('passdisplay').innerHTML = document.getElementById("InputPassword").value;
+}
